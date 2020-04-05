b36444f622ec791dfe0ed92a05e1d0c7f7ab2586
carloswimmer <coiote777@gmail.com>
2020-04-04 23:17:53 -0300

    Use Insomnia

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
+app.use(express.json())
+
+app.post('/users', (request, response) => {
+  // const query = request.query
+  // const params = request.params
+  const body = request.body
+
+  console.log(body)
+
   return response.json({
     evento: 'Omnistack 11.0',
     nome: 'Carlos Wimmer'
